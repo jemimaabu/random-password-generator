4046dd577014de843a195a9080b3e62e07e3c129
Adam Clydesdale <aclydesdale@users.noreply.github.com>
2019-01-27 12:50:50 +0000

    Fixed issue #2

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ var includeLetters = document.getElementById("include-letters");
 var randomPassword = document.getElementById("random-password");
 var errorMessage = document.getElementById("error-message");
 
-var characters = ["&", "/", "\\", "#", ",", "+", "(", ")", "$", "~", "%", ".", "'", ":", "*", "?", "<", ">", "/", "{", "}", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 0,1,2,3,4,5,6,7,8,9]
+var defaultCharacters = ["&", "/", "\\", "#", ",", "+", "(", ")", "$", "~", "%", ".", "'", ":", "*", "?", "<", ">", "/", "{", "}", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z","A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", 0,1,2,3,4,5,6,7,8,9]
+var characters = [];
 var passwordArray = [];
 
 
 function generatePassword() {
     randomPassword.innerHTML = "";
     passwordArray=[];
+    characters = defaultCharacters;
     if (!specialCharacters.checked) {
         characters = characters.join("").replace(/[&\/\\#,+()$~%.':*?<>{}]/g,'').split('')
     } 
